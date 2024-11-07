eedce503829192adeb2e474f581bd84f2d765754
Herman Lindalen Nodeland <afs0027473@agderskolen.no>
2024-11-07 21:05:59 +0100

    removed react-speech library from project. will need to find another react-speech library caused problems in build removed library from project and from component will need to find new similar library

--- src/components/LydSpiller.tsx
+++ src/components/LydSpiller.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import Speech from 'react-speech';
 
 interface LydSpillerProps {
     text: string;
@@ -9,16 +8,6 @@ interface LydSpillerProps {
 export default function LydSpiller({ text, className }: LydSpillerProps) {
     return (
         <div>
-            <Speech 
-                text={text}
-                textAsButton={true}
-                displayText="Spill Lyd"
-                voice="no-NO"
-                pitch={1}
-                rate={0.8}
-                volume={1}
-                lang="no-NO"
-            />
             <p className={className}>{text}</p>
         </div>
     );
